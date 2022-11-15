7842ed865de49a165c6e285329baa62cbe7e6241
jefferyfan <jefferyfan@DESKTOP-A25MONL.localdomain>
2022-11-15 01:44:30 -0600

    feature extractor

--- extractor.py
+++ extractor.py
@@ -1,14 +1,16 @@
 import cv2
 import numpy as np
+from skimage.measure import ransac
+from skimage.transform import FundamentalMatrixTransform
 
 class Extractor(object):
     GX = 16//2
     GY = 12//2
     
     def __init__(self):
-        self.orb = cv2.ORB_create(100)
+        self.orb = cv2.ORB_create()
         self.last = None
-        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        self.bf = cv2.BFMatcher(cv2.NORM_HAMMING)
 
     def extract(self, img):
         # Attempt at SIFT (problem too slow)
@@ -37,11 +39,26 @@ class Extractor(object):
         kps, des = self.orb.compute(img, kps)
         
         #matching
-        matches = None
+        ret = []
         
         if self.last is not None:
-            matches = self.bf.match(des, self.last['des'])
-            #print(matches)
+            matches = self.bf.knnMatch(des, self.last['des'], k = 2)
+            for m, n in matches:
+                if m.distance < 0.75 * n.distance:
+                    kp1 = kps[m.queryIdx].pt
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    ret.append((kp1, kp2))
+        
+        if len(ret) > 0:
+            ret = np.array(ret)
+            print(ret.shape)
+
+        #filter
+            model, inliers = ransac((ret[:, 0], ret[:, 1]), FundamentalMatrixTransform,
+                                     min_samples = 8, residual_threshold = 1, max_trials = 100)
+            #print(sum(inliers))
+            ret = ret[inliers]
+
         self.last = {'kps': kps, 'des': des}
         #print(type(corners))
-        return kps, des, matches
+        return ret
--- slam.py
+++ slam.py
@@ -2,6 +2,7 @@
 
 import cv2
 import numpy as np
+
 from extractor import Extractor
 
 W = 1920 // 2
@@ -11,20 +12,27 @@ cv2.namedWindow('image', cv2.WINDOW_NORMAL)
 orb = cv2.ORB_create()
 #print(dir(orb))
 
-
-
-        
-        
-
 fe = Extractor()
 
 def process_frame(img):
     img = cv2.resize(img, (W, H))
-    kps, des, matches = fe.extract(img)
+    matches = fe.extract(img)
     #kps, des = orb.detectAndCompute(img,None)
-    for p in kps:
-        u, v = map(lambda x: int(round(x)), p.pt)
-        cv2.circle(img, (u,v), color = (0, 255, 0), radius = 3)
+
+    print("matches %d" % ((len(matches))))
+
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        cv2.line(img, (u1,v1), (u2, v2), color = (255, 0, 0))
+        cv2.circle(img, (u1,v1), color = (0, 255, 0), radius = 2)
+        cv2.circle(img, (u2,v2), color = (0, 255, 0), radius = 2)
+
+
+
+    # for p in kps:
+    #     u, v = map(lambda x: int(round(x)), p.pt)
+    #     cv2.circle(img, (u,v), color = (0, 255, 0), radius = 3)
 
     # for p in kps:
     #     u, v = map(lambda x: int(round(x)), p[0])
